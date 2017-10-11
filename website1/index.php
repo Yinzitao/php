44bc0ba3f5895366e33df27d5a7029ac5ad0fff0
lanou3g <365391752@qq.com>
2017-10-11 17:40:06 +0800

    add filt

---
+++ website1/index.php
@@ -0,0 +1,14 @@
+<?php include "./inc/header.php" ?>
+	<h1>Home</h1>
+<?php include "./inc/footer.php" ?>
+
+<!-- 
+	include & require的区别
+
+	如果有报错信息,include会显示报错信息,但不影响后面的代码信息
+
+	如果有报错信息,require会显示报错信息,但后面的代码不会执行
+
+	include_once & require_once的作用
+	重复引入,只识别一次!	
+ -->
